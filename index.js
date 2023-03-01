7508c53be560436b97401dac3291aa7de21f2d22
Chhatrapati Mandwe <mandwec12@gmail.com>
2023-03-01 13:24:07 +0530

    add

--- index.js
+++ index.js
@@ -1,53 +1,81 @@
 const display = document.getElementById('display');
 const startbtn = document.getElementById('start');
+const pausebtn = document.getElementById('pause');
 const resetbtn = document.getElementById('reset');
-const pausebtn = documnet.getElementById('pause');
 const castbtn = document.getElementById('cast');
-let miliseconds = 0;
+const parentdiv = document.querySelector('.parentdiv')
+let m = 0;
 let sec = 0;
-let mins = 0;
+let min = 0;
 let hr = 0;
-let interval = null;
-startbtn.addEventListener('click',start);
-pausebtn.addEventListener('click',pause);
-resetbtn.addEventListener('click', reset);
-function startTimer(){
-    miliseconds++;
-    if(miliseconds === 1000){
-        miliseconds = `${000}`
+let interval;
+startbtn.addEventListener('click',()=>{
+   pausebtn.disabled = false;
+   resetbtn.disabled = false;
+   castbtn.disabled = false;
+   startbtn.disabled = true;
+   interval = setInterval(() => {
+    m++
+    if(m === 1000){
         sec++
+        m = "000";
         if(sec === 60){
-            sec = `${00}`
-            mins++
-            if(mins === 60){
-                mins = `${00}`
+            min++;
+            if(min < 10){
+                min = "0" + min
+            }
+            sec = "00"
+            if(min === 60){
                 hr++
+                min = "00"
             }
         }
     }
-    display.innerHTML = `${hr} : ${mins} : ${sec} : ${miliseconds}`
-}
-function start(){
-    // pausebtn.disabled = false;
-    // resetbtn.disabled = false;
-    // startbtn.disabled = true;
-    if(interval){
-        return
+    let ms;
+    let hrs;
+    let mins;
+    let secs;
+    if(m < 10){
+        ms = "00" + m
     }
-    interval = setInterval(startTimer, 1);
-}
-
-function pause(){
+    if(m > 9){
+        ms = "0" + m
+    }
+    if(m > 99){
+        ms = m
+    }
+    if(sec < 10){
+        secs = "0" + sec
+    }
+    if(min < 10){
+        mins = "0" + min
+    }
+    if(hr < 10){
+        hrs = "0" + hr
+    }
+    display.innerHTML = `${hrs} : ${mins} : ${secs} : ${ms}`
+   }, 1);
+});
+pausebtn.addEventListener('click',()=>{
+    pausebtn.disabled = true;
+    resetbtn.disabled = false;
+    castbtn.disabled = true;
+    startbtn.disabled = false;
     clearInterval(interval);
-    interval = null;
-    // if(pausebtn.innerText)
-}
-
-function reset(){
-    pause()
-    miliseconds = 0;
-    sec = 0;
-    mins = 0;
-    hr = 0;
+});
+resetbtn.addEventListener('click', ()=>{
+    pausebtn.disabled = true;
+    resetbtn.disabled = true;
+    castbtn.disabled = true;
+    startbtn.disabled = false;
+    clearInterval(interval);
+    m = 0;
     display.innerHTML = "00 : 00 : 00 : 000"
-}
+});
+let count = 0
+castbtn.addEventListener('click', ()=>{
+    count++
+    let p = document.createElement('p');
+    parentdiv.appendChild(p);
+    p.innerHTML = `cast ${count}:- ` + display.innerHTML
+})
